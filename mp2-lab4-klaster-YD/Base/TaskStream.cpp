25cbfd0c56598aee6bc34792fbe8e62d6841c380
DDYunin <DaniilUnin2002@yandex.ru>
2021-11-23 11:46:10 +0300

    Ready. Version 1.0

---
+++ mp2-lab4-klaster-YD/Base/TaskStream.cpp
@@ -0,0 +1,25 @@
+#include "TaskStream.h"
+
+Task TaskStream::CreateNewTask(int _time_app, int _id, int num_proc)
+{
+    Task ts;
+    int duration = rand() % 10 + 1;
+    int number_proccesors = rand() % num_proc + 1;
+    //int time_exe = duration + _time_app;
+    ts.PutAll(_id, _time_app, duration, number_proccesors);
+    return ts;
+}
+
+bool TaskStream::IsTask()
+{
+    double probability = static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+    if (probability < TaskIntåns)
+        return true;
+    else
+        return false;
+}
+
+void TaskStream::ChangeTimeForEndTask(Task& ts, int _index)
+{
+    ts.DetermineTimeExecutin(_index);
+}
